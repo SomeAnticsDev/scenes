1e7fbe44146ef65c663d6250fca343d9715e0f5d
Ben Myers <ben.myers@okstate.edu>
2021-05-19 17:08:52 -0500

    GAAD AMA

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,6 +1,7 @@
 module.exports = {
-	title: `Getting Started with Automated Accessibility Testing with Lucia Cerchie`,
+	// title: `Let's Build an Open Source Web Accessibility Resource!`,
+	title: `Ask Me Anything About Accessibility! #GAAD`,
 	guestTwitters: [
-		'CerchieLucia'
+		
 	]
 }
