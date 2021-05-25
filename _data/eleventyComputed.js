7a05dcf0e0d6785ea1ff9802eb6b927d186cd764
Ben Myers <ben.myers@okstate.edu>
2021-05-25 08:05:23 -0500

    a11ysurf title

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,6 +1,5 @@
 module.exports = {
-	// title: `Let's Build an Open Source Web Accessibility Resource!`,
-	title: `Ask Me Anything About Accessibility! #GAAD`,
+	title: `Let's Build an Open Source Web Accessibility Resource!`,
 	guestTwitters: [
 		
 	]
