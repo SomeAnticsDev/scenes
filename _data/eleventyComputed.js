f4b19f1e2534e24fad15f71ecd02c6e025f3c762
Ben Myers <ben.myers@okstate.edu>
2021-06-06 14:47:08 -0500

    Chan's GitHub nav stream

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,6 +1,8 @@
 module.exports = {
-	title: `Let's Build a UI with Figma, with Martin Bavio`,
+	// title: `Let's Build a UI with Figma, with Martin Bavio`,
+	title: `Let's Clone GitHub's Navigation Widget with Chantastic`,
 	guestTwitters: [
-		'_marbiano'
+		// '_marbiano'
+		'chantastic'
 	]
 }
