51c19bef7c5bd999a3a624b5f6a6b3942d9f51a4
Ben Myers <ben.myers@okstate.edu>
2021-07-18 12:43:45 -0500

    Consume SomeAntics.dev upcoming streams API to populate title and guests

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,6 +1,34 @@
-module.exports = {
-	title: `Auditing Websites for Accessibility with Anna E. Cook`,
-	guestTwitters: [
-		'annaecook'
-	]
+const fetch = require('node-fetch');
+
+module.exports = async () => {
+	/**
+	 * @type {{
+	 * 		events: {
+	 * 			title: string,
+	 * 			fullTitle: string,
+	 * 			hosts: {
+	 * 				name: string,
+	 * 				pronouns: string,
+	 * 				avatar: string,
+	 * 				instagram?: string,
+	 * 				twitch?: string,
+	 * 				twitter?: string,
+	 * 				website?: string
+	 * 			}[],
+	 * 			date: string,
+	 * 			slug: string,
+	 * 			url: string
+	 * 		}[]
+	 * 	}}
+	 */
+	const upcomingStreams = await fetch('https://someantics.dev/api/upcomingStreams.json').then(res => res.json());
+	const [nextStream] = upcomingStreams.events;
+
+	return {
+		title: nextStream.fullTitle || nextStream.title,
+		guestTwitters: nextStream
+			.hosts
+			.filter(host => host.twitter && host.name !== 'Ben Myers')
+			.map(host => host.twitter)
+	}
 }
