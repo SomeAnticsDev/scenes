6552a9d70eef983af4dabbe27a890d44ae598ea2
Ben Myers <ben.myers@okstate.edu>
2022-08-23 12:29:38 -0500

    Add one-off stream

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -6,15 +6,7 @@ module.exports = async () => {
 	 * 		events: {
 	 * 			title: string,
 	 * 			fullTitle: string,
-	 * 			hosts: {
-	 * 				name: string,
-	 * 				pronouns: string,
-	 * 				avatar: string,
-	 * 				instagram?: string,
-	 * 				twitch?: string,
-	 * 				twitter?: string,
-	 * 				website?: string
-	 * 			}[],
+	 * 			hosts: Host[],
 	 * 			date: string,
 	 * 			slug: string,
 	 * 			url: string
@@ -22,22 +14,64 @@ module.exports = async () => {
 	 * 	}}
 	 */
 	const upcomingStreams = await fetch('https://someantics.dev/api/upcomingStreams.json').then(res => res.json());
-	const [nextStream] = upcomingStreams.events;
+	// const [nextStream] = upcomingStreams.events;
 
-	// const nextStream = {
-	// 	title: `Accessibility Office Hours`,
-	// 	date: '2022-06-07T14:00:00.000',
-	// 	hosts: []
-	// }
+	const nextStream = {
+		title: `Help me build the Some Antics website!`,
+		date: '2022-08-23T14:00:00.000',
+		hosts: []
+	}
 
 	// console.log(nextStream)
 
 	return {
 		title: nextStream.fullTitle || nextStream.title,
+		shortTitle: nextStream.title,
 		streamTime: nextStream.date,
+		// formattedHosts: formatHosts(nextStream.hosts),
 		guestTwitters: nextStream
 			.hosts
 			.filter(host => host.twitter && host.name !== 'Ben Myers')
 			.map(host => host.twitter)
 	}
 }
+
+/**
+ * 
+ * @param {Host[]} hosts 
+ */
+function formatHosts(hosts) {
+	if (!hosts) return '';
+
+	/** @type {Host[]} */
+	let orderedHosts = [...hosts];
+	if (orderedHosts[0].name == 'Ben Myers') {
+		const [ben, ...guests] = orderedHosts;
+		orderedHosts = [...guests, ben];
+	}
+
+	switch (orderedHosts.length) {
+		case 1:
+			return orderedHosts[0].name.bold();
+		case 2:
+			return `${orderedHosts[0].name.bold()} and ${orderedHosts[1].name.bold()}`;
+		default:
+			const bolded = orderedHosts.map(host => host.name.bold());
+			const lastIndex = bolded.length - 1;
+			const lastName = bolded[lastIndex];
+			bolded[lastIndex] = 'and ' + lastName;
+			return bolded.join(',&nbsp;');
+	}
+}
+
+/**
+ * @typedef {{
+ * 		name: string,
+ * 		pronouns: string,
+ * 		avatar: string,
+ * 		instagram?: string,
+ * 		twitch?: string,
+ * 		twitter?: string,
+ * 		website?: string
+ * }} Host
+ */
